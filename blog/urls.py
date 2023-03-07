7609264933666911daeb35e36acc9451dbff5bd7
Denis Pablo <denispablo17@gmail.com>
2023-03-07 14:31:07 -0300

    extends

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "blog"
+
 urlpatterns = [
     path("", views.render_post, name="posts"),
-    path("<int:post_id>", views.detalles_post),
+    path("<int:post_id>", views.detalles_post, name="post_detail"),
 ]
